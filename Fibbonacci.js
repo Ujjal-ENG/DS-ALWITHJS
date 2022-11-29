5be062c669d7877bb3a1e8492939de049111d11c
user <ujjalroy7862@gmail.com>
2022-11-30 00:57:55 +0600

    done this problem with n timecomplexity, now try to resolve it

---
+++ Fibbonacci.js
@@ -0,0 +1,11 @@
+const fibo = (n) => {
+  const fib = [0, 1]
+  for (let i = 2; i < n; i++){
+    fib[i] = fib[i-1] + fib[i-2]
+  }
+  return fib
+};
+
+console.log(fibo(2));
+console.log(fibo(5));
+console.log(fibo(7));
